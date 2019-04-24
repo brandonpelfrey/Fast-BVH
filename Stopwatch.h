3fe15cd52d2bbad07e81ec02bd0cc9f6123eb90e
ychding11 <ych_ding@163.com>
2019-04-24 18:07:20 +0800

    add build support on Windows Platform.

--- Stopwatch.h
+++ Stopwatch.h
@@ -1,6 +1,7 @@
 #ifndef Stopwatch_h
 #define Stopwatch_h
 
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
 #include <sys/time.h>
 
 class Stopwatch {
@@ -17,5 +18,29 @@ public:
 	void reset() { start = _stopwatch(); }
 	double read() const {	return _stopwatch() - start; }
 };
+#else
 
+#include <numeric>
+#include <chrono>
+class Stopwatch {
+private:
+    std::chrono::time_point<std::chrono::system_clock> start;
+    std::chrono::time_point<std::chrono::system_clock>  _stopwatch() const
+    {
+        return std::chrono::system_clock::now();
+    }
+
+public:
+    Stopwatch() { reset(); }
+    void reset() { start = _stopwatch(); }
+
+    // return second
+    double read() const
+    {
+        std::chrono::duration<double> diff = _stopwatch() - start;
+        return diff.count();
+    }
+};
+
+#endif
 #endif
