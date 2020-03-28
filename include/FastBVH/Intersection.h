d297bee7b9268b51a94c3d3c2f555ad81205127f
Taylor Holberton <taylorcholberton@gmail.com>
2020-03-28 18:11:53 -0400

    Including UV coordinates

--- include/FastBVH/Intersection.h
+++ include/FastBVH/Intersection.h
@@ -19,6 +19,8 @@ struct Intersection final {
   const Primitive* object = nullptr;
   //! The normal at the point of intersection.
   Vec3 normal = { 0, 0, 1 };
+  //! The UV coordinates at the position of intersection.
+  Float uv[2] = { 0, 0 };
   //! Gets the position at the ray hit the object.
   //! \param ray_pos The ray position.
   //! \param ray_dir The ray direction.
